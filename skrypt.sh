6546a2145d682ad0d7cf11a8a997cfb3dabb85f1
Paweł Jacyk <paweljacyk@gmail.com>
2022-07-02 09:38:33 +0200

    dodanie flagi --logs do skrypt.sh

--- skrypt.sh
+++ skrypt.sh
@@ -3,6 +3,14 @@ while [ "$1" != "" ]; do
 	--date)
 		shift
 		date;;
+	--logs)
+		shift
+		for i in `seq 1 100`; do
+			touch log$i.txt
+			echo "log$i.txt">>log$i.txt
+			echo "skrypt.sh">>log$i.txt
+			echo date>>log$i.txt
+		done;;
 	*)
 		;;
 	esac
