fe2099a1309e7a6964ed6cc44936caf989dda60a
Paweł Jacyk <paweljacyk@gmail.com>
2022-07-02 10:33:02 +0200

    dodanie obsługi argumentu dla flagi logs do skrypt.sh

--- skrypt.sh
+++ skrypt.sh
@@ -5,11 +5,18 @@ while [ "$1" != "" ]; do
 		date;;
 	--logs)
 		shift
-		for i in `seq 1 100`; do
+		if [ -z "$1" ]
+		then
+			a=100
+		else
+			a=$1
+		fi
+		for i in `seq 1 $a`; do
 			touch log$i.txt
 			echo "log$i.txt">>log$i.txt
 			echo "skrypt.sh">>log$i.txt
 			echo date>>log$i.txt
+		shift
 		done;;
 	*)
 		;;
